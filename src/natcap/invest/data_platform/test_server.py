4614ad5b5ded005dbfa7b0331e822d42afac3891
Rich Sharp <richpsharp@gmail.com>
2015-10-08 18:06:50 -0700

    re #3418 skeleton for fetch data tile

--- src/natcap/invest/data_platform/test_server.py
+++ src/natcap/invest/data_platform/test_server.py
@@ -17,6 +17,14 @@ def main():
     data_preview = data_server.get_data_preview()
     open('data_preview.zip', 'wb').write(data_preview)
 
+    bounding_box = [
+        94.99958345439518, 25.00041712672993, 100.00041678772851,
+        19.999583793396596]
+    data_id = "3d921d0fe6d31d2a76fa1e3923098bf6ee0f62da"
+    data_tile = data_server.fetch_data_tile(bounding_box, data_id)
+    print data_tile
+    open('data_tile.zip', 'wb').write(data_tile)
+
 
 if __name__ == '__main__':
     main()
