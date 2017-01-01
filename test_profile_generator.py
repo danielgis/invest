70bf48ee01501bb94568b33c09adc783107024e7
Rich Sharp <richpsharp@gmail.com>
2016-12-31 21:50:13 -0800

    re #3609 adding smoothing to profile analysis

--- test_profile_generator.py
+++ test_profile_generator.py
@@ -15,9 +15,10 @@ def main():
         'workspace_dir': 'profile_generator_workspace',
         'results_suffix': 'adaptive_steps',
         #'bathymetry_path': r"C:\Users\rpsharp\Documents\clipped_claybark.tif",
-        'bathymetry_path': r"C:\Users\rpsharp\Documents\bitbucket_repos\invest\data\invest-data\Base_Data\Marine\DEMs\claybark_dem",
+        'bathymetry_path': r"clipped_claybark_dem.tif",
+        #'bathymetry_path': r"E:\repositories\bitbucket_repos\invest\data\invest-data\Base_Data\Marine\DEMs\claybark_dem",
         'shore_height': 0.0,  # shore elevation on bathy layer
-        'representative_point_vector_path': r"C:\Users\rpsharp\Dropbox\shared_with_users\profile_data_for_jess\representative_profile_points.shp",
+        'representative_point_vector_path': r"D:\Dropbox\shared_with_users\profile_data_for_jess\representative_profile_points.shp",
         # stepsize is (close distance step, max close distance)
         # stepsize is (far distance step, far distance definition)
         'step_size': ((10, 500), (100, 2000)),
@@ -25,7 +26,7 @@ def main():
         'offshore_profile_length': 2000,
         'onshore_profile_length': 500,
         'habitat_vector_path_list': [
-            (r"sample_claybark_hab_a.shp", 'name')],
+            (r"D:\Dropbox\shared_with_users\profile_data_for_jess\sample_claybark_hab_a.shp", 'name')],
     }
     natcap.invest.profile_generator.execute(args)
 
